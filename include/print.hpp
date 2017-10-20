db0d3a5344557bfcc5be97d7f4e88a596e9d855a
pettro98 <ya.skywalker@yandex.ru>
2017-10-21 00:00:22 +0300

    added doxygen.conf

--- include/print.hpp
+++ include/print.hpp
@@ -2,7 +2,17 @@
 #include <fstream>
 #include <iostream>
 /*!
+\brief function that prints text to stream
+ \param text string to print to output stream
+ \param output stream (default value = cout)
+ \returns this function doesn't return a value
  */
 void print(const std::string& text, std::ostream& out = std::cout);
+/*!
+\brief function that prints text to file
+ \param text string to print to file
+ \param output file stream
+ \returns this function doesn't return a value
+ */
 void print(const std::string& text, std::ofstream& out);
 
